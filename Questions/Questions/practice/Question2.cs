556806f38061ba9f2af4348617c8347ee58bd071
Ahsan Saqib <ahsansaqib808@gmail.com>
2017-08-30 19:59:03 +0500

    Question2

---
+++ Questions/Questions/practice/Question2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.practice
+{
+    class Question2
+    {
+        public void Solution2(int a , int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("a + b =  {0}",sum);
+        }
+    }
+}
